74c3322a482b5e87ca0af075911523df05fddbb8
Sandhya Ratan <Sandhya.Ratan@philips.com>
2023-05-01 18:27:10 +0530

    done Bfs dfs

---
+++ Graph/BFS/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFS
+{
+    class Program
+    {
+        List<int> bfsOfGraph(int V, List<List<int>> adj)
+        {
+            List<bool> visited = new List<bool>();
+            List<int> ans = new List<int>();
+
+            for (int i = 0; i < V; i++)
+            {
+                visited.Add(false);
+            }
+
+            bfs(0, adj, visited, ans);
+            return ans;
+        }
+
+        private void bfs(int u, List<List<int>> adj, List<bool> visited, List<int> ans)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(u);
+            visited[u] = true;
+            ans.Add(u);
+            while (queue.Count > 0)
+            {
+                int a = queue.Dequeue();
+                foreach (var v in adj[a])
+                {
+                    if (!visited[v])
+                    {
+                        queue.Enqueue(v);
+                        visited[v] = true;
+                        ans.Add(v);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<List<int>> adj = new List<List<int>>();
+            //V = 5 , adj = [[1,2,3] , [], [4], [], []]
+            adj.Add(new List<int> { 1, 2, 3 });
+            adj.Add(new List<int> { });
+            adj.Add(new List<int> { 4 });
+            adj.Add(new List<int> { });
+            adj.Add(new List<int> { });
+
+            Program program = new Program();
+            program.bfsOfGraph(5, adj);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Graph/DFS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFS
+{
+    class Program
+    {
+        public
+  List<int> dfsOfGraph(int V, List<List<int>> adj)
+        {
+            List<bool> visited = new List<bool>();
+            List<int> ans = new List<int>();
+            //Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < V; i++)
+            {
+                visited.Add(false);
+            }
+
+            dfs(0, adj, visited, ans);
+            return ans;
+        }
+
+        void dfs(int u, List<List<int>> adj, List<bool> visited, List<int> ans)
+        {
+            if (visited[u])
+                return;
+            visited[u] = true;
+            ans.Add(u);
+            foreach (var v in adj[u])
+            {
+                if (!visited[v])
+                    dfs(v, adj, visited, ans);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<List<int>> adj = new List<List<int>>();
+            //V = 5 , adj = [[2, 3, 1] , [0], [0,4], [0], [2]]
+            adj.Add(new List<int> { 2, 3, 1 });
+            adj.Add(new List<int> { 0 });
+            adj.Add(new List<int> { 0, 4 });
+            adj.Add(new List<int> { 0 });
+            adj.Add(new List<int> { 2 });
+
+            Program program = new Program();
+            program.dfsOfGraph(5, adj);
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
